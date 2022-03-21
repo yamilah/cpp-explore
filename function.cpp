f245e5ecbff7d28d62d11bea62073c52972c6352
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-03-20 19:49:18 -0700

    Initial commit

---
+++ function.cpp
@@ -0,0 +1,3 @@
+void nothingness() {
+  std::cout << "hey boooooogie";
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "function.cpp"
+
+int main() {
+  std::cout << "hey boogie\n";
+  nothingness();
+  return 0;
+}
