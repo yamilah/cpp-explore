4b092db2fdfe916b1a900d71b9b5b628ed038f27
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-04-30 21:01:49 -0700

    removed old program stuff

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-clang++ function.cpp main.cpp \
+clang++ *.cpp \
 -std=c++20 -lraylib -L./raylib -Iraylib \
 -framework CoreVideo \
 -framework IOKit \
 -framework Cocoa \
 -framework GLUT \
 -framework OpenGL \
--o ligma.exe.elon.420.exe
+-o ligma.exe.elon.420.exe \
+&& ./ligma.exe.elon.420.exe
--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "function.h"
 #include "raylib.h"
 
 int main() {
-  Person* people = new Person[9999999];
-
   InitWindow(600, 600, "ligma");
   while(!WindowShouldClose()) {
     BeginDrawing();
@@ -12,17 +9,4 @@ int main() {
 
     EndDrawing();
   }
-
-  Person p;
-  p.SetAge(22);
-  p.SetName("j");
-  p.PrintInformation();
-
-  Person b;
-  if (p.IsCompatible(&b)) {
-    std::cout << "boolin" << std::endl;
-  } else {
-    std::cout << p.GetName() << " is an old fart." << std::endl;
-  }
-  return 0;
 }
